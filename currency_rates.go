6da9482220b3faa688d28c4fa2f1a2d46f8141a1
Atasik <Atasikgames@gmail.com>
2023-07-25 15:09:47 +0300

    fix

--- currency_rates.go
+++ currency_rates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/xml"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -20,8 +21,29 @@ const (
 	path     = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=%s"
 )
 
-type userSession struct {
+type Curs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Valute  []struct {
+		CharCode string `xml:"CharCode"`
+		Name     string `xml:"Name"`
+		Value    string `xml:"Value"`
+	}
+}
+
+type Client struct {
 	client http.Client
+}
+
+func newClient(timeout time.Duration) (*Client, error) {
+	if timeout <= 0 {
+		return nil, errors.New("invalid timeout")
+	}
+
+	return &Client{
+		client: http.Client{
+			Timeout: timeout,
+		},
+	}, nil
 }
 
 func main() {
@@ -33,54 +55,80 @@ func main() {
 
 	flag.Parse()
 
-	// code = "USD"
-	// dateStr = "2002-03-02"
+	dateStr, err := formatDate(dateStr)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	c, err := newClient(timeout)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	result, err := c.getCurs(dateStr)
+	if err != nil {
+		print("failed to get curs")
+		return
+	}
+
+	found := false
+	for _, v := range result.Valute {
+		if v.CharCode == code {
+			fmt.Printf("%s (%s) %s", code, v.Name, v.Value)
+			found = true
+		}
+	}
+	if !found {
+		fmt.Print("nothing found")
+	}
+}
+
+func formatDate(dateStr string) (string, error) {
 	date, err := time.Parse(YYYYMMDD, dateStr)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return "", errors.New("date invalid format")
 	}
 
 	newDate := date.Format(DDMMYYYY)
 
 	dateStr = strings.Replace(newDate, "-", "/", 2)
+	return dateStr, nil
+}
 
-	u := &userSession{
-		client: http.Client{
-			Timeout: timeout,
-		},
-	}
-
-	var result ValCurs
-
+func (c *Client) getCurs(dateStr string) (Curs, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf(path, dateStr), nil)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return Curs{}, err
 	}
 	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
 	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
-	if xmlBytes, err := u.getXML(req); err != nil {
-		fmt.Printf("Failed to get XML: %v", err)
-		return
+
+	var result Curs
+	if xmlBytes, err := c.getXML(req); err != nil {
+		return Curs{}, err
 	} else {
-		decoder := xml.NewDecoder(bytes.NewReader(xmlBytes))
-		decoder.CharsetReader = charset
-		if err := decoder.Decode(&result); err != nil {
-			fmt.Printf("[ERROR] Cannot decode file %e", err)
-			return
+		if err := decodeXML(xmlBytes, &result); err != nil {
+			return Curs{}, err
 		}
 	}
 
-	for _, v := range result.Valute {
-		if v.CharCode == code {
-			fmt.Printf("%s (%s) %s", code, v.Name, v.Value)
-		}
-	}
+	return result, err
 }
 
-func (u *userSession) getXML(req *http.Request) ([]byte, error) {
-	resp, err := u.client.Do(req)
+func decodeXML(xmlBytes []byte, v any) error {
+	decoder := xml.NewDecoder(bytes.NewReader(xmlBytes))
+	decoder.CharsetReader = charset
+	if err := decoder.Decode(&v); err != nil {
+		fmt.Printf("[ERROR] Cannot decode file %e", err)
+		return err
+	}
+	return nil
+}
+
+func (c *Client) getXML(req *http.Request) ([]byte, error) {
+	resp, err := c.client.Do(req)
 	if err != nil {
 		return []byte{}, fmt.Errorf("GET error: %v", err)
 	}
@@ -96,15 +144,6 @@ func (u *userSession) getXML(req *http.Request) ([]byte, error) {
 	}
 
 	return data, nil
-}
-
-type ValCurs struct {
-	XMLName xml.Name `xml:"ValCurs"`
-	Valute  []struct {
-		CharCode string `xml:"CharCode"`
-		Name     string `xml:"Name"`
-		Value    string `xml:"Value"`
-	}
 }
 
 func charset(charset string, input io.Reader) (io.Reader, error) {
